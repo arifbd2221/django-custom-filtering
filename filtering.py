bbd4711d824c141f9af76a402a008d659dd18dbd
arifbd2221 <mohidulhoque216@gmail.com>
2025-01-22 13:08:04 +0600

    feat: Initialize project with parse_search_phrase function definition

---
+++ filtering.py
@@ -0,0 +1,11 @@
+import re
+
+def parse_search_phrase(allowed_fields, phrase):
+    """
+    This function Parses a search phrase and converts it into a Django Q object.
+
+    :param allowed_fields: List of allowed fields for filtering. eg: date, distance
+    :param phrase: The search phrase string.
+    :return: A Q object representing the filters.
+    """
+    pass

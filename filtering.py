598b4eee9d40842b051acc8b531d77e187dcf267
arifbd2221 <mohidulhoque216@gmail.com>
2025-01-22 14:46:20 +0600

    feat: Add operator mapping and test input to see the tokenization

--- filtering.py
+++ filtering.py
@@ -12,4 +12,22 @@ def parse_search_phrase(allowed_fields, phrase):
     # tokenizing search phrases
     token_pattern = re.compile(r'(\(|\)|\band\b|\bor\b|\w+\s(eq|ne|gt|lt)\s[^\s\)]+)', re.IGNORECASE)
 
-    
+    # Mapping of operators
+    operator_mapping = {
+        'eq': '',       # Equals
+        'ne': '__ne',   # Not equals
+        'gt': '__gt',   # Greater than
+        'lt': '__lt'    # Less than
+    }
+
+    tokens = token_pattern.findall(phrase)
+    print(f"{tokens=}") # tokens=[('(', ''), ('date eq 2016-05-01', 'eq'), (')', ''), ('AND', ''), ('(', ''), ('(', ''), ('distance gt 20', 'gt'), (')', ''), ('OR', ''), ('(', ''), ('distance lt 10', 'lt'), (')', ''), (')', '')]
+
+
+
+
+if __name__ == '__main__':
+    allowed_fields = ['date', 'distance']
+    search_phrase = "(date eq 2016-05-01) AND ((distance gt 20) OR (distance lt 10))"
+
+    parse_search_phrase(allowed_fields, search_phrase)
